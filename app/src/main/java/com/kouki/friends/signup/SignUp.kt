d4931f739b31f358c6431de2ef306061fd046287
kouki4416 <pank4416@gmail.com>
2022-09-25 08:37:56 +0900

    Improved signup screen UI

--- app/src/main/java/com/kouki/friends/signup/SignUp.kt
+++ app/src/main/java/com/kouki/friends/signup/SignUp.kt
@@ -1,14 +1,17 @@
 package com.kouki.friends.signup
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.integerResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.kouki.friends.R
 import java.io.StringReader
 
@@ -16,10 +19,22 @@ import java.io.StringReader
 @Preview
 fun SignUp() {
     Column(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
     ) {
-        Text(text = "Create an account")
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = stringResource(R.string.createAnAccount),
+                style = TextStyle(fontSize = integerResource(id = R.integer.h4).sp)
+            )
+        }
+        Spacer(modifier = Modifier.height(16.dp))
         OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
             value = "",
             label = {
                 Text(text = stringResource(id = R.string.email))
@@ -27,13 +42,18 @@ fun SignUp() {
             onValueChange = {}
         )
         OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
             value = "",
             label = {
                 Text(text = stringResource(id = R.string.password))
             },
             onValueChange = {}
         )
-        Button(onClick = { }) {
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(
+            modifier = Modifier.fillMaxWidth(),
+            onClick = { }
+        ) {
             Text(text = stringResource(id = R.string.signUp))
         }
     }
