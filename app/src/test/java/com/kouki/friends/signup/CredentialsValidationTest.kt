76f7ac4a50f14c397ddc20689c8c792b5757f711
kouki4416 <pank4416@gmail.com>
2022-09-25 15:40:51 +0900

    Can detect different invalid email formats

--- app/src/test/java/com/kouki/friends/signup/CredentialsValidationTest.kt
+++ app/src/test/java/com/kouki/friends/signup/CredentialsValidationTest.kt
@@ -4,13 +4,23 @@ import com.kouki.friends.InstantTaskExecutorExtension
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 @ExtendWith(InstantTaskExecutorExtension::class)
 class CredentialsValidationTest {
-    @Test
-    fun invalidEmail() {
+    @ParameterizedTest
+    @CsvSource(
+        "'email'",
+        "'a@b.c'",
+        "'ab@b.c'",
+        "'ab@bc.c'",
+        "''",
+        "'   '",
+    )
+    fun invalidEmail(email: String) {
         val viewModel = SignUpViewModel()
-        viewModel.createAccount("email", ":password:", ":about:")
+        viewModel.createAccount(email, ":password:", ":about:")
         assertEquals(SignUpState.BadEmail, viewModel.signUpState.value)
     }
 
