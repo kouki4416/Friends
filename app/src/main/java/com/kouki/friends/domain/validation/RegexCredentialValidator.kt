1e76f3a5f4d01ac9899ab37c1734e730b1588b21
kouki4416 <pank4416@gmail.com>
2022-09-26 18:28:51 +0900

    Removed duplication

--- app/src/main/java/com/kouki/friends/domain/validation/RegexCredentialValidator.kt
+++ app/src/main/java/com/kouki/friends/domain/validation/RegexCredentialValidator.kt
@@ -3,23 +3,31 @@ package com.kouki.friends.domain.validation
 import java.util.regex.Pattern
 
 class RegexCredentialValidator {
-        fun validate(
-            email: String,
-            password: String
-        ): CredentialsValidationResult {
-            val emailRegex =
-                """[a-zA-Z0-9+._%\-]{1,64}@[a-zA-Z0-9][a-zA-Z0-9\-]{1,64}(\.[a-zA-Z0-9][a-zA-Z0-9\-]{1,25})"""
-            val emailPattern = Pattern.compile(emailRegex)
-            val passwordRegex = """^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*+=\-]).{8,}$"""
-            val passwordPattern = Pattern.compile(passwordRegex)
 
-            val result = if (!emailPattern.matcher(email).matches()) {
-                CredentialsValidationResult.InvalidEmail
-            } else if (!passwordPattern.matcher(password).matches()) {
-                CredentialsValidationResult.InvalidPassword
-            } else {
-                CredentialsValidationResult.Valid
-            }
-            return result
+    private companion object {
+        private const val EMAIL_REGEX =
+            """[a-zA-Z0-9+._%\-]{1,64}@[a-zA-Z0-9][a-zA-Z0-9\-]{1,64}(\.[a-zA-Z0-9][a-zA-Z0-9\-]{1,25})"""
+        private const val PASSWORD_REGEX =
+            """^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*+=\-]).{8,}$"""
+    }
+
+    private val emailPattern = Pattern.compile(EMAIL_REGEX)
+    private val passwordPattern = Pattern.compile(PASSWORD_REGEX)
+
+    fun validate(
+        email: String,
+        password: String
+    ): CredentialsValidationResult {
+        val emailPattern = emailPattern
+        val passwordPattern = passwordPattern
+
+        val result = if (!emailPattern.matcher(email).matches()) {
+            CredentialsValidationResult.InvalidEmail
+        } else if (!passwordPattern.matcher(password).matches()) {
+            CredentialsValidationResult.InvalidPassword
+        } else {
+            CredentialsValidationResult.Valid
         }
+        return result
+    }
 }
