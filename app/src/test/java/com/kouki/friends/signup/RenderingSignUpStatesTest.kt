3d7b4951ecade1dc191859db26c053f71b95248d
kouki4416 <pank4416@gmail.com>
2022-10-09 12:40:47 +0900

    Improved test by removing details

--- app/src/test/java/com/kouki/friends/signup/RenderingSignUpStatesTest.kt
+++ app/src/test/java/com/kouki/friends/signup/RenderingSignUpStatesTest.kt
@@ -5,21 +5,20 @@ import com.kouki.friends.domain.user.InMemoryUserCatalog
 import com.kouki.friends.domain.user.User
 import com.kouki.friends.domain.user.UserRepository
 import com.kouki.friends.domain.validation.RegexCredentialValidator
-import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 
 @ExtendWith(InstantTaskExecutorExtension::class)
 class RenderingSignUpStatesTest {
+    private val userRepository = UserRepository(InMemoryUserCatalog())
+    private val viewModel = SignUpViewModel(RegexCredentialValidator(), userRepository)
+    private val tom = User("tomId", "tom@friends.com", "about Tom")
 
     @Test
-    fun uiStatesAreDeliveredInParticularOrder(){
-        val userRepository = UserRepository(InMemoryUserCatalog())
-        val viewModel = SignUpViewModel(RegexCredentialValidator(), userRepository)
-        val tom = User("tomId", "tom@friends.com", "about Tom")
+    fun uiStatesAreDeliveredInParticularOrder() {
         val deliveredStates = mutableListOf<SignUpState>()
-        viewModel.signUpState.observeForever{ deliveredStates.add(it) }
+        viewModel.signUpState.observeForever { deliveredStates.add(it) }
 
         viewModel.createAccount(tom.email, "P@ssWord1", tom.about)
 
